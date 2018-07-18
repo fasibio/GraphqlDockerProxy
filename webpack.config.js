1bea4610194375bd2027b57179999b39c883ec5c
Fabian Simon <fabian.simon@hornbach.com>
2018-07-18 11:43:53 +0200

    Current not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const Dotenv = require('dotenv-webpack')
-var FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin')
 
 
 module.exports = {
-  entry: ['babel-polyfill', './index.js'],
+  entry: './index.js',
   target: 'node',
   output: {
     path: __dirname + '/dist', //eslint-disable-line
@@ -15,45 +14,15 @@ module.exports = {
         test: /\.js$/,
         use: ['remove-hashbag-loader'],
       },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              'transform-class-properties',
-              'transform-flow-comments',
-            ],
-          },
-        },
-      },
     ],
     loaders: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: [
-      //     /node_modules/,
-      //   ],
-      //   loader: 'babel',
-      //   // options: {
-      //   //   plugins: [
-      //   //     'transform-es2015-arrow-functions',
-      //   //     'check-es2015-constants',
-      //   //     'transform-es2015-block-scoping',
-      //   //     'syntax-async-functions',
-      //   //     'transform-regenerator',
-      //   //     'transform-class-properties',
-      //   //     'transform-flow-strip-types',
-      //   //     ['transform-runtime', {
-      //   //       'polyfill': true,
-      //   //       'regenerator': true,
-      //   //     }],
-      //   //   ],
-      //   //   presets: ['env', 'es2015'],
-      //   // },
-      // },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+        ],
+        loader: 'babel-loader',
+      },
     ],
   },
   resolveLoader: {
@@ -65,6 +34,7 @@ module.exports = {
     new Dotenv({
       path: './.env',
     }),
+    // new FlowBabelWebpackPlugin(),
 
   ],
 }
