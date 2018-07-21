6ba34259ee0a1fd7a506a4e6dc2f587f7a521749
Fabian_Simon <fasibio@gmail.com>
2018-07-21 09:20:28 +0200

    added more dockerfinder tests

--- src/finder/dockerFinder/__tests__/dockerFinder.test.js
+++ src/finder/dockerFinder/__tests__/dockerFinder.test.js
@@ -6,6 +6,18 @@ jest.mock('../../endpointsAvailable', () => {
     },
   }
 })
+jest.mock('../loadBalancer', () => {
+  return {
+    closeAllServer: () => {},
+    loadANewLoadBalaceMiddleware: (listOfBackends) => {
+      return {
+        url: 'http://127.0.0.1:8001',
+        clients: listOfBackends.map((one) => Object.assign({}, one)),
+      }
+
+    },
+  }
+})
 jest.mock('../../../properties', () => {
   return {
     token: () => {
@@ -37,6 +49,39 @@ describe('Tests the docḱerfinder Class', () => {
       expect(endpoints).toEqual(newEndpoints)
     })
 
+    it ('tests there need loadbalance endpoints change ', async() => {
+      const endpoints = await dockerFinder.getEndpoints()
+      endpoints['one'].push({
+        url: 'http://124.122.123.123:8000/graphql',
+        namespace: 'one',
+        typePrefix: 'one_',
+        created: '20180101',
+        __imageID: 'one',
+      })
+      const newEndpoints = await dockerFinder.handleRestart(endpoints)
+      expect(newEndpoints['one'][0]).toMatchSnapshot()
+
+    })
+
 
   })
+  describe('tests updateUrl ', () => {
+    it('by absolut url', () => {
+      const url = 'https://test.de/graphql'
+      expect(dockerFinder.updateUrl(url, {})).toBe(url)
+    })
+    it('by relativ url', () => {
+      const sockData = {
+        NetworkSettings: {
+          Networks: {
+            'web': {
+              IPAddress: '127.0.0.1',
+            },
+          },
+        },
+      }
+      expect(dockerFinder.updateUrl(':3000/graphql', sockData)).toBe('http://127.0.0.1:3000/graphql')
+    })
+  })
+
 })
