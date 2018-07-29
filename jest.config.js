ee7091d57bf8d00acaceeef054d49e373ad6cef4
Fabian_Simon <fasibio@gmail.com>
2018-07-29 11:23:00 +0200

    WIP

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 require('./src/idx')
 
 module.exports = {
+  verbose: true,
+  testURL: 'http://localhost/',
+  collectCoverage: true,
+  'coverageReporters': ['json', 'html'],
+  coverageDirectory: '/coverage',
+  'collectCoverageFrom': [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
   globals: {
-    testURL: 'http://localhost/',
     idx: global.idx,
-    collectCoverage: true,
-    'coverageReporters': ['json', 'html'],
-    coverageDirectory: '/coverage',
-    'collectCoverageFrom': [
-      '**/*.{js,jsx}',
-      '!**/node_modules/**',
-      '!**/vendor/**',
-    ],
   },
 }
