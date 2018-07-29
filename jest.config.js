da823d1b4d7946e7883ad8e85fba24b134d357cf
Fabian_Simon <fasibio@gmail.com>
2018-07-29 11:26:48 +0200

    FIX Coverage

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,14 @@ require('./src/idx')
 module.exports = {
   verbose: true,
   testURL: 'http://localhost/',
-  collectCoverage: true,
-  'coverageReporters': ['json', 'html'],
+  // collectCoverage: true,
+  // 'coverageReporters': ['json', 'html'],
   coverageDirectory: 'coverage',
-  'collectCoverageFrom': [
-    '**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-  ],
+  // 'collectCoverageFrom': [
+  //   '**/*.{js,jsx}',
+  //   '!**/node_modules/**',
+  //   '!**/vendor/**',
+  // ],
   globals: {
     idx: global.idx,
   },
