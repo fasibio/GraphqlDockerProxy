655bb6d357ddb597bf1afa74c2545123f4876287
Fabian_Simon <fasibio@gmail.com>
2018-08-03 21:57:23 +0200

    WIP

--- sonarexec.sh
+++ sonarexec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sonar-scanner -Dsonar.projectKey=graphqldockerproxy -Dsonar.sources=. -Dsonar.host.url=https://sonar.server2.fasibio.de -Dsonar.login=$sonarqubelogin -Dsonar.testExecutionReportPaths=reports/test-reporter.xml -Dsonar.test.exclusions=**.test.js
+sonar-scanner -Dsonar.projectKey=graphqldockerproxy -Dsonar.sources=. -Dsonar.host.url=https://sonar.server2.fasibio.de -Dsonar.login=$sonarqubelogin
