08bd59fdbe449f9b4878c6468543d99e69409cae
Fabian_Simon <fasibio@gmail.com>
2018-08-03 22:01:41 +0200

    WIP

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ require('./src/idx')
 module.exports = {
   verbose: true,
   testURL: 'http://localhost/',
-  'testResultsProcessor': 'jest-sonar-reporter',
   // collectCoverage: true,
   // 'coverageReporters': ['json', 'html'],
   coverageDirectory: 'coverage',
