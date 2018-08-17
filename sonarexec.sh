919bf5a20f590e3745ad7d464cc4cc0b2e2d4f70
Fabian Simon <fabian.simon@hornbach.com>
2018-08-17 13:54:57 +0200

    WIP

--- sonarexec.sh
+++ sonarexec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sonar-scanner -Dsonar.projectKey=graphqldockerproxy.$CI_COMMIT_REF_NAME -Dsonar.sources=. -Dsonar.host.url=https://sonar.server2.fasibio.de -Dsonar.login=$sonarqubelogin
+sonar-scanner -Dsonar.projectKey=graphqldockerproxy_$CI_COMMIT_REF_NAME -Dsonar.sources=. -Dsonar.host.url=https://sonar.server2.fasibio.de -Dsonar.login=$sonarqubelogin
